da611a26a8e38c63e869c0c3681d2c2687bf09ee
Nicholas Thomson <neekthomson@gmail.com>
2017-08-09 13:00:50 -0400

    Code style changes for campfire demo

--- campfire_demo.R
+++ campfire_demo.R
@@ -1,22 +1,27 @@
 source("campfire_lib.R")
 
 campfireApp(
+  
   controller = div(
-    h1("Super Awesome Controller"),
+    h1("Controller"),
     selectInput(inputId = "fruitSelection",
-                label = "Pick a fruit",
-                choices = c("Apple", "Banana", "Pear"))
+      label = "Pick a fruit",
+      choices = c("Apple", "Banana", "Pear"))
   ),
+  
   wall = div(
-    h1("Super Awesome Wall"),
+    h1("Wall"),
     textOutput("wallText")
   ),
+  
   floor = div(
-    h1("Super Awesome Floor"),
+    h1("Floor"),
     textOutput("floorText")
   ),
+  
   serverFunct = function(serverValues, output) {
     output$wallText <- renderText({ serverValues$fruitSelection })
     output$floorText <- renderText({ serverValues$fruitSelection })
   }
+  
 )
